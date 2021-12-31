eb10de1eeeeab119c7becb2b6100d6aa16f013b9
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-31 00:18:32 -0500

    refact: change to module imports

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
